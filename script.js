60951ec362ee524cd5328c1484d7c23a4c257ff3
Allyse Hettinger <51002304+allyseh@users.noreply.github.com>
2020-10-17 16:11:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ window.onload=function(){
 	.then(function(response) {
 	      return response.json();
 	}).then(function(json) {
-	      let results = "";
+	      let results = '<h3>Definiiton(s):</h3>';
 	      results += '<ol>';
 	      for (let i = 0; i < json.definitions.length; i++) {
 		      results += '<li>(' + json.definitions[i].partOfSpeech + ')  ' + json.definitions[i].definition + '</li>';
@@ -27,6 +27,26 @@ window.onload=function(){
 	})
 	.catch(err => {
 		console.log(err);
+	});
+		  
+	fetch("https://rapidapi.p.rapidapi.com/words/" + value + "/synonyms", {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+			"x-rapidapi-key": "b0c0d90426msh5005b0082a5e613p1e300ejsn688ca2b295d5"
+		}
+	}).then(function(json) {
+	      let results = '<h3>Synonyms:</h3>';
+	      results += '<p>';
+	      for (let i = 0; i < json.synonyms.length - 1; i++) {
+		      results += json.synonyms[i] + ', ';
+	      }
+	      results += json.synonyms[json.synonyms.length - 1];
+              results += '</p>';
+	      document.getElementById("definition").innerHTML = results;    
+	})
+	.catch(err => {
+		console.error(err);
 	});
 		  
   });
